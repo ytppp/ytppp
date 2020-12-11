df622d8e5f93761114bda9971db46f9e9b33dcac
ytppp <651003708@qq.com>
2020-12-11 09:40:58 +0800

    Modify config

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master # 推送到github
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
